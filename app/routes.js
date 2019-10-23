6230fc704286da3219b0d9d7d4463b60eddeb2aa
Alvaro Corrales <alvarito1050@gmail.com>
2019-10-23 15:17:31 -0500

    [resolves #1] register evaluation

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const PracticesController = require('../app/controllers/PracticesController');
+const EvaliationController = require('../app/controllers/EvaliationController');
 
 const router = express.Router();
 
 router.get('/students/practices', PracticesController.getStudentsWithPractice);
+
+router.post('/evaluation/regiterController', EvaliationController.registerEvaluation);
 
 module.exports = router;
