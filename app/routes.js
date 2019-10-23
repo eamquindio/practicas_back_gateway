ee52105bd14e06e5a2230a4c4b93469e26b6ff34
diegolt8 <diegoalugo@hotmail.com>
2019-10-23 15:16:38 -0500

    [resolve #7] gateway homologation

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const PracticesController = require('../app/controllers/PracticesController');
 const ConvocatoryController = require('../app/controllers/ConvocatoryController');
+const HomologationController = require('../app/controllers/HomologationController');
 
 const router = express.Router();
 
 router.get('/students/practices', PracticesController.getStudentsWithPractice);
 /* Convocatory Routes */
 router.put('/convocatorias/:id(\\d+)', ConvocatoryController.modifyAnnouncement);
+
+/* Homologación Routes */
+router.post('/homologation/:id(\\d+)/signup', HomologationController.signUpHomologation);
 
 module.exports = router;
