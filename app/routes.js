8c6e85b9930e17cf66f7b490941bc998c692abb3
Jhon Quirama <quirama80@gmail.com>
2019-10-25 22:35:52 -0500

    [resolves #11] setAppoinment

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,7 @@ const PracticesController = require('../app/controllers/PracticesController');
 const ConvocatoryController = require('../app/controllers/ConvocatoryController');
 const PracticeRequestController = require('../app/controllers/PracticeRequestController');
 const HomologationController = require('../app/controllers/HomologationController');
+const SetAppoimentController = require('./controllers/SetAppoimentController');
 
 const router = express.Router();
 
@@ -10,6 +11,8 @@ router.get('/students/practices', PracticesController.getStudentsWithPractice);
 /* Convocatory Routes */
 router.put('/convocatorias/:id(\\d+)', ConvocatoryController.modifyAnnouncement);
 router.post('/convocatory/:id(\\d+)/signup', ConvocatoryController.signUp);
+/* Set Appoiment Routes */
+router.post('/psicologia/setAppointment', SetAppoimentController.appoimentSet);
 
 /* Homologación Routes */
 router.post('/homologation/:id(\\d+)/signup', HomologationController.signUpHomologation);
