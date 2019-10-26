faacc980035a7f47dddb0d0de95af6e2c895ec44
Jhon Quirama <quirama80@gmail.com>
2019-10-25 22:18:43 -0500

    [resolves #11] setAppoinment

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const PracticesController = require('../app/controllers/PracticesController');
 const ConvocatoryController = require('../app/controllers/ConvocatoryController');
+const SetAppoimentController = require('./controllers/SetAppoimentController');
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.get('/students/practices', PracticesController.getStudentsWithPractice);
 /* Convocatory Routes */
 router.put('/convocatorias/:id(\\d+)', ConvocatoryController.modifyAnnouncement);
 router.post('/convocatory/:id(\\d+)/signup', ConvocatoryController.signUp);
+/* Set Appoiment Routes */
+router.post('/psicologia/setAppointment', SetAppoimentController.appoimentSet);
 
 module.exports = router;
