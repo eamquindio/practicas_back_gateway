909c4794595da1a45ff51295805addefcf45109e
m9n0lo <xthernux@gmail.com>
2019-10-26 11:28:40 -0500

    correcion conflictos

--- app/routes.js
+++ app/routes.js
@@ -40,7 +40,7 @@ router.put('/appointment', AppointmenController.modifyAppointmen);
 router.put('/request_company/:id(\\d+)', PracticeRequestController.editStatus);
 router.put('/request_student/:id(\\d+)', PracticeRequestController.editStatus);
 
-router.post('/practices/incioPractices',PracticesController.registerPractices)
+router.post('/practices/start_practices',PracticesController.registerPractices)
 
 
 module.exports = router;
