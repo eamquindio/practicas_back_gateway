91c4c6b4ea91d33ca3d1e626226be5789dc14a76
ING. Criss Sinisterra Rivera TNR <you@example.com>
2019-11-02 09:51:13 -0500

    [resolves #3] requestCompany

--- app/controllers/RequestCompanyController.js
+++ app/controllers/RequestCompanyController.js
@@ -2,14 +2,12 @@ const RequestCompanyController = module.exports;
 const RequestCompanyService = require('../services/request/RequestCompanyService');
 
 RequestCompanyController.registerCompanyRequest = async (req, res, next) => {
-    const { body } = req;
-    try {
-      const result = await RequestCompanyService.registerCompanyRequest(body);
-  
-      return res.send(result);
-    } catch (error) {
-      console.log({ error });
-  
-      return next(error);
-    }
-  };
+  const { body } = req;
+  try {
+  const result = await RequestCompanyService.registerCompanyRequest(body);
+    return res.send(result);
+  } catch (error) {
+    console.log({ error });
+    return next(error);
+  }
+};
--- app/services/request/RequestCompanyService.js
+++ app/services/request/RequestCompanyService.js
@@ -1,5 +1,5 @@
 const RequestCompanyService = module.exports;
 const PracticeRequestMSResource = require('../../resources/PracticeRequestMSResource');
 
-RequestCompanyService.registerRequestCompany = requestCompany => 
-    PracticeRequestMSResource.registerRequestCompany(requestCompany);
+RequestCompanyService.registerRequestCompany = requestCompany =>
+  PracticeRequestMSResource.registerRequestCompany(requestCompany);
