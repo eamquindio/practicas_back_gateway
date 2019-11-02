a844ced1a141ee4e56ba90bb6c6e15082a4964a2
ING. Criss Sinisterra Rivera TNR <you@example.com>
2019-11-02 09:59:56 -0500

    [resolves #3] requestCompany

--- app/controllers/RequestCompanyController.js
+++ app/controllers/RequestCompanyController.js
@@ -4,10 +4,12 @@ const RequestCompanyService = require('../services/request/RequestCompanyService
 RequestCompanyController.registerCompanyRequest = async (req, res, next) => {
   const { body } = req;
   try {
-  const result = await RequestCompanyService.registerCompanyRequest(body);
+    const result = await RequestCompanyService.registerCompanyRequest(body);
     return res.send(result);
   } catch (error) {
     console.log({ error });
+
     return next(error);
   }
+
 };
