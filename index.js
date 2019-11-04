5a279a1796e5532b550d81d858b406bedd2df00b
Daryl110 <daryl.8220@gmail.com>
2019-11-04 18:51:39 -0500

    [resolves #34] dependency implemented cors for request of angular

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./app/routes');
 const ErrorHandlerMiddleware = require('./app/utils/ErrorHandlerMiddleware');
+const cors = require('cors');
 
 const app = express();
 const { PORT = 3000 } = process.env;
 
 app.use(bodyParser.json());
+
+app.use(cors());
 
 app.use('/api/pasantiaseam', routes);
 
