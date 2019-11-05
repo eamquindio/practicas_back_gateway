8e2056829d8393dbca5af567d6deb79cb2eeeabf
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-11-05 08:27:16 -0500

    add gateway

--- app/resources/ConvocatoryMSResource.js
+++ app/resources/ConvocatoryMSResource.js
@@ -2,7 +2,7 @@ const ConvocatoryMSResource = module.exports;
 const HTTPClient = require('../utils/HTTPClient');
 const { MICROSERVICE_URL } = require('../configs/microservices');
 
-const BASE_URL = `${MICROSERVICE_URL}/api/convocatoria/`;
+const BASE_URL = `${MICROSERVICE_URL}/api/convocatoria`;
 
 ConvocatoryMSResource.init = () => {
   console.log({ BASE_URL, HTTPClient });
