7262b185ae1950d43916943ab83d745a59ae0f08
Camilo Andres Ferrer Bustos <camilo.ferrer@rappi.com>
2019-11-05 08:29:55 -0500

    add gateway

--- app/resources/EnterprisesMSResource.js
+++ app/resources/EnterprisesMSResource.js
@@ -2,7 +2,7 @@ const EnterprisesMSResource = module.exports;
 const HTTPClient = require('../utils/HTTPClient');
 const { MICROSERVICE_URL } = require('../configs/microservices');
 
-const BASE_URL = `${MICROSERVICE_URL}/api/empresas/`;
+const BASE_URL = `${MICROSERVICE_URL}/api/empresas`;
 
 EnterprisesMSResource.init = () => {
   console.log({ BASE_URL, HTTPClient });
