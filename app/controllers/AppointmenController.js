e1595d998ac929b50d888bb11b1d8d011a2502ee
Jhon Quirama <quirama80@gmail.com>
2019-11-06 19:46:53 -0500

    [resolves #41] bug fixed

--- app/controllers/AppointmenController.js
+++ app/controllers/AppointmenController.js
@@ -1,10 +1,24 @@
 const AppointmenController = module.exports;
 const AppointmenService = require('../services/psicology/AppointmenService');
+const SetAppoimentService = require('../services/psicology/SetAppoimentService');
 
 AppointmenController.modifyAppointmen = async (req, res, next) => {
   const { body } = req;
   try {
     const result = await AppointmenService.modifyAppointmen(body);
+
+    return res.send(result);
+  } catch (error) {
+    console.log({ error });
+
+    return next(error);
+  }
+};
+
+SetAppoimentController.appoimentSet = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const result = await SetAppoimentService.appoimentSet(body);
 
     return res.send(result);
   } catch (error) {
--- app/services/psicology/SetAppoimentService.js
+++ app/services/psicology/SetAppoimentService.js
@@ -4,7 +4,7 @@ const StudentMSResource = require('../../resources/StudentsMSResource');
 const { ErrorHandler } = require('../../utils/ErrorHandlerMiddleware');
 
 SetAppoimentService.appoimentSet = async (appoiment) => {
-  const studentToValidate = await StudentMSResource.get(appoiment.estudiante_id);
+  const studentToValidate = await StudentMSResource.get(appoiment.estudianteId);
   console.log(studentToValidate);
 
   if (!studentToValidate) throw ErrorHandler.BaseError('student not exists', 409);
